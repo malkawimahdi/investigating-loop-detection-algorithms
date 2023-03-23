9253efa4323d6ca1443c62c6af3dac6876dbeef8
Mahdi Malkawi <106409817+malkawimahdi@users.noreply.github.com>
2023-03-23 14:57:16 +0000

    Updated algorithm-1
    Updated algorithm-1 to include final changes to include utilised code snippets and to include significant real world space optimisations using bitsets instead of maps.

--- algorithms/algorithm-1/recursive-depth-first-search-back-edge-detection/Graph.cpp
+++ algorithms/algorithm-1/recursive-depth-first-search-back-edge-detection/Graph.cpp
@@ -1,18 +1,32 @@
 //
+// NOTICE: Graph.cpp utilises a significant proportion of code snippets and general structure
+    // with no specified license located at:
+// Title: Detect Cycle in a Directed Graph
+// Author: GeeksForGeeks
+// Year: 2023
+// URL: https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
+// URL Date: 22rd March 2023
+//
 // Created by Mahdi Malkawi on 08/02/2023.
 // Graph.cpp
 //
 
 #include "Graph.h"
 
-// Constructor takes number of nodes and generates a list containing an entry the size of nodes.
-// (Doubly Linked List)
+// This constructor is derived from (GeeksForGeeks, 2023), specifically the generation of how the diagraph is
+    // represented as an adjacency list.
+// Constructor takes number of nodes and generates a DLL for each node containing an entry the size of nodes.
+// Reserves the maximum space that the bitset MAY require.
 Graph::Graph(int nodes)
 {
     this->nodes = nodes;
+    this->visited.reserve(this->nodes);
+    this->current_visit.reserve(this->nodes);
     this->adjacent_nodes = new std::list<int>[nodes];
 }
 
+// This function is derived from (GeeksForGeeks, 2023) with the difference of limiting the number of adjacent nodes.
+    // Size of 1 is used as it references up to two nodes.
 // Add an edge to current graph in the directed form: currentNode -> adjacentNode.
 // Follows the property of a Control Flow Graph (CFG), whereby each node can have AT MOST two other adjacent nodes.
 void Graph::addEdge(int current_node, int adjacent_node)
@@ -27,6 +41,8 @@ void Graph::addEdge(int current_node, int adjacent_node)
     }
 }
 
+// This function is derived from (GeeksForGeeks, 2023), with the difference of outputting the Head and
+    // back edge for detected cycles and implementing more efficient data structures of bitsets.
 // Implementation of Depth First Search (DFS) from a given node.
 bool Graph::recursiveDepthFirstSearch(int &node)
 {
@@ -59,42 +75,34 @@ bool Graph::recursiveDepthFirstSearch(int &node)
     return false;
 }
 
+// This function is derived from (GeeksForGeeks, 2023), with the difference in what occurs if a cycle is detected,
+    // specifically counting the number of cycles and the efficiency improvements of a bitset.
 bool Graph::containsCycle(void)
 {
     unsigned int cycle_count = 0;
-
-    // Initalise variables inside graph to false.
-    for (auto &[key, value] : this->visited)
-    {
-        value = false;
-    }
-
-    for (auto &[key, value] : this->current_visit)
-    {
-        value = false;
-    }
 
     // For each node, check if visited.
     for (int counter = 0; counter < this->nodes; ++counter)
     {
         // If a node has not been traversed, and it's traversal returns true then return true;
         // Utilises that the compiler, will determine if the first condition is true or false, before proceeding
-        // to check the second condition. (Only saves a line of code)
+        // to check the second condition.
         if (!this->visited[counter] && recursiveDepthFirstSearch(counter))
         {
             ++cycle_count;
         }
+
     }
 
     if (cycle_count)
     {
-        std::cout << "Cycles: " << cycle_count << std::endl;
+        std::cout << "Cycle(s): " << cycle_count << std::endl;
 
         return true;
     }
     else
     {
-        std::cout << "No Cycle Detected!";
+        std::cout << "No Cycle(s) Detected!";
         return false;
     }
 }
--- algorithms/algorithm-1/recursive-depth-first-search-back-edge-detection/Graph.h
+++ algorithms/algorithm-1/recursive-depth-first-search-back-edge-detection/Graph.h
@@ -7,25 +7,24 @@
 #define GRAPH_H
 
 #include <list>
-#include <map>
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 
 class Graph
 {
 private:
-    int nodes;                         // Number of nodes within graph.
-    std::list<int> *adjacent_nodes;    // Pointer to list containing adjacent nodes for all nodes. (Doubly Linked List)
-    std::map<int, bool> visited;       // Key value pair. (Key: Node) (Value: hasNodeBeenVisited)
-    std::map<int, bool> current_visit; // Key value pair. (Key: Node) (Value: hasNodeBeenVisited) differs from visited
-                                       // as it keep track of the key value pairs outside of function call.
+    unsigned int nodes;                  // Number of nodes within graph.
+    std::list<int> *adjacent_nodes;     // Pointer to list containing adjacent nodes for all nodes. (Doubly Linked List)
+    std::vector<bool> visited;          // Vector bitset checking for visited nodes.
+    std::vector<bool> current_visit;    // Vector bitset checking for back edges.
 
     bool recursiveDepthFirstSearch(int &node); // Implementation of Depth First Search (DFS) from a given node.
 
 public:
     explicit Graph(int nodes);                         // Constructor, which takes the number of nodes for a graph.
     void addEdge(int current_node, int adjacent_node); // Add an edge to current graph currentNode -> adjacentNode
-    bool containsCycle(void);                          // Uses private function "depthFirstSearch" to locate back-edges and detect loops.
+    bool containsCycle(void);                          // Initialising function to detect back edge loops.
 };
 
 #endif // GRAPH_H
